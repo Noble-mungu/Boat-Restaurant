82e2be95385d2ab9be66bffc6f526c2ecc826c32
Noble Mungu <mungunoble@gmail.com>
2023-05-08 18:44:22 +0300

    add reservation

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -20,6 +20,33 @@ namespace BOAT_RESTAURANT
 
                
             );
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Login",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Customers", action = "Index", id = UrlParameter.Optional }
+
+
+            );
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Reservation",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Reservations", action = "Index", id = UrlParameter.Optional }
+
+
+            );
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Admin",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Admins", action = "Index", id = UrlParameter.Optional }
+
+
+            );
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace BOAT_RESTAURANT.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+           
 
             return View();
         }
